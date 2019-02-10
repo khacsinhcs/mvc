bf6cc4e36eb800994a2df57e8bda86a98ded089d
khacsinhcs <khacsinhcs@gmail.com>
2019-02-11 05:59:35 +0700

    avoid import :D, it could be have many another cases like this one

---
+++ test/com/alab/core/CaseClassSpec.scala
@@ -0,0 +1,28 @@
+package com.alab.core
+
+import com.alab.conf._
+import com.alab.model.{HasValuesMapperHelper, MapValues}
+import org.scalatest.{FlatSpec, Matchers}
+
+import scala.collection.immutable.HashMap
+
+class CaseClassSpec extends FlatSpec with Matchers {
+
+  val student = MapValues(HashMap(
+    "first_name" -> "Sinh",
+    "age" -> "18"
+  ))
+
+  case class StudentClazz(firstName: Option[String], age: Int)
+
+  object Student extends Type(n = "Student", des = "Student") {
+    val first_name: Field[String] = f("first_name", StringType)
+    val age: Field[Int] = f("age", IntType)
+  }
+
+  "hasValues" should "convert to class" in {
+    val otherStudent = HasValuesMapperHelper.materialize[StudentClazz](student, Student)
+    otherStudent.firstName should be(Some("Sinh"))
+  }
+
+}
