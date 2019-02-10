71e153329fd56081df0aa0de6298afb911026512
khacsinhcs <khacsinhcs@gmail.com>
2019-02-10 09:05:25 +0700

    handle materialize case class from map

--- app/com/alab/mvc/action/AsJson.scala
+++ app/com/alab/mvc/action/AsJson.scala
@@ -1,6 +1,5 @@
 package com.alab.mvc.action
 
-import com.alab.Mappable
 import com.alab.model.{HasValues, MapValues}
 import com.alab.mvc.model.{JsValues, PathValue}
 import javax.inject.Inject
@@ -16,8 +15,6 @@ class BodyAsJson @Inject()(val parser: BodyParsers.Default)(implicit val executi
   override protected def transform[A](request: Request[A]): Future[RequestAsJson[A]] = Future.successful {
     val header = request.headers.toSimpleMap
     val headersAsHasValue = MapValues(header)
-
-    val map = Mappable[RequestAsJson]
 
 
     request.body match {
